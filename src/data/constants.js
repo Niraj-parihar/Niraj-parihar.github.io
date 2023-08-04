8bcd50cbdbf01d7c462bedf14b28bc5c1d8e25ab
Niraj <nirajp.tt.19@nitj.ac.in>
2023-08-04 20:29:36 +0530

    updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Niraj-parihar",
   resume:
-    "https://drive.google.com/file/d/1Cu3DrCK6jpe8B0wXjztqWNXamz6nNIDm/view?usp=drive_link",
+    "https://drive.google.com/file/d/14bkN2aFGttQysyNWy3SlQFvxXrhi-SWl/view?usp=sharing",
   insta: "https://www.instagram.com/_thegr8niraj/",
 };
 
